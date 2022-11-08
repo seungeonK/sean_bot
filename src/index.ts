ce6bc9093f653d7991329926efcb3445d4fed441
Seungeon Kim <hjk6281@gmail.com>
2022-11-07 20:17:16 -0500

    implement bot login

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+import { token } from './config.json';
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+client.once(Events.ClientReady, c => {
+    console.log(typeof c);
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+})
 client.on('ready', () => {
     console.log(`${client.user}`);
 });
 
-console.log(process.env);
+// Log in to Discord with your client's token
+client.login(token);
